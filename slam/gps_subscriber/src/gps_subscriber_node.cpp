0770d69be0e4fda51732c40d14fd15352f9ea89c
ohjw420 <ohjw420@gmail.com>
2020-07-14 23:49:32 +0900

    parsing gps topic sentence

--- slam/gps_subscriber/src/gps_subscriber_node.cpp
+++ slam/gps_subscriber/src/gps_subscriber_node.cpp
@@ -2,12 +2,26 @@
 #include "nmea_msgs/Sentence.h"
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 void msgCallback(const nmea_msgs::Sentence::ConstPtr& msg) {
-    string st = msg->sentence;
-
-    if (st.substr(1, 5) == "GNGGA") {
+    string s = msg->sentence;
+    string delimiter = ",";
+    vector<string> tokens;
+    string token;
+    size_t pos = 0;
+    
+    while ((pos = s.find(delimiter)) != std::string::npos) {
+        token = s.substr(0, pos);
+        tokens.push_back(token);
+        //std::cout << token << std::endl;
+        s.erase(0, pos + delimiter.length());
+    }
+    if(tokens[0] == "$GNGGA"){
+        cout<<tokens[1] << " " << tokens[2] << " " << tokens[4] << endl;
+    }
+    /*if (st.substr(1, 5) == "GNGGA") {
         string time;
         string lat;
         string lon;
@@ -17,7 +31,7 @@ void msgCallback(const nmea_msgs::Sentence::ConstPtr& msg) {
         lon = st.substr(32, 13);
 
         cout << time << " " << lat << " " << lon << endl;
-    }
+    }*/
 }
 
 int main(int argc, char** argv) {
