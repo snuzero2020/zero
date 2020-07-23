6f02095fc6bcca6d9fceefbc2d465919b3e8f113
Dongha Hwang <depth221@gmail.com>
2020-07-23 12:40:15 +0900

    Revert "push map_cutter"
    This reverts commit 7f25e3d0840504cad58c8b86ade70a12162b18cf.

--- slam/src/mapping/main.cpp
+++ slam/src/mapping/main.cpp
@@ -1,20 +1,6 @@
 #include "map_cutter.h"
 #include <iostream>
 
-#include "opencv2/core.hpp"
-#include "opencv2/highgui.hpp"
-
-#define FMTC 1
-#define KCity 2
-
-using namespace std;
-using namespace cv;
-
 int main(int argc, char** argv) {
-    MapCutter map_cutter(FMTC);
-
-    double lat = 0;
-    double lon = 0;
-    double heading = 0.5;
-    Mat modified_map(map_cutter.smartCut(lat, lon, heading));
+    std::cout << "1" << std::endl;
 }
--- slam/src/mapping/map_cutter.h
+++ slam/src/mapping/map_cutter.h
@@ -1,33 +1,5 @@
-#ifndef MAPCUTTER_H
-#define MAPCUTTER_H
-
-#include <iostream>
 #include "opencv2/opencv.hpp"
-#include "ros/ros.h"
-#include <string>
-#include <vector>
 
-#define FMTC 1
-#define KCity 2
-
-using namespace std;
 using namespace cv;
 
-
-class MapCutter {
-    private:
-        int number_of_maps;
-        vector<Mat> map_list();
-        vector<double> position_xy(2);
-        vector<int> position_px(2);
-        int place;
-    public:
-        MapCutter();
-        MapCutter(int place);
-
-        int cutViaPxCenter(Mat& original_map, Mat& modified_map, int pixel_x, int pixel_y);
-        Mat smartCut(double lat, double lon, double heading);
-        int cut(Mat& original_map, Mat& modified_map, double lat, double lon, double heading);
-};
-
-#endif
+int cut_map(Mat& img, double x, double y, double heading);
