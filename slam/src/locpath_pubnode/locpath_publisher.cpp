9ace76d95e06e16c92e85674a6e202af95b03f80
YoonhoShin <davidthe0213@gmail.com>
2020-07-30 00:03:43 +0900

    local costmap publisher and path planning visualizer for debuging initial commit

--- slam/src/locpath_pubnode/locpath_publisher.cpp
+++ slam/src/locpath_pubnode/locpath_publisher.cpp
@@ -26,7 +26,7 @@ class Local_path{
 		
 		//cv::Mat local_path_img = cv::Mat(300,300, CV_8UC3, cv::Scalar(255,255,255));
 		//set path to the saved global path image
-		cv::Mat glob_path = cv::imread("glob_path.png");
+		cv::Mat glob_path = cv::imread("/home/parallels/data/glob_path.png");
 
 		void callback(const slam::Data data){
 			geometry_msgs::PoseStamped loc_pose;
