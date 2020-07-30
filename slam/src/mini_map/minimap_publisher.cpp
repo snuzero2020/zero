5c3ac0af8d88db1c7c7722937c371f7598e7ee59
greg3566 <greg3566@gmail.com>
2020-07-31 05:08:20 +0900

    Simplify minimap_publisher with opencv slicing

--- slam/src/mini_map/minimap_publisher.cpp
+++ slam/src/mini_map/minimap_publisher.cpp
@@ -36,51 +36,52 @@ class map_tracer{
 		void callback(const slam::Data data){
 			int inst_pixel_x, inst_pixel_y;
 			int copy_pixel_x{}, copy_pixel_y{};
-                        //inst_pixel_x = data.x;
-		        //inst_pixel_y = data.y;
+            // inst_pixel_x = data.x;
+			// inst_pixel_y = data.y;
 			XYToPixel(inst_pixel_x,inst_pixel_y,data.x,data.y);
 			std::cout<<inst_pixel_x << inst_pixel_y << std::endl;
 			bool x_500{inst_pixel_x <= 500}, y_500{inst_pixel_y <= 500}, x_14500{inst_pixel_x >= 14500}, y_14500{inst_pixel_y >= 14500};
 			std::cout << x_500 << "," << y_500 << "," << x_14500 << "," << y_14500 << std::endl;	
 
 			if(count == 0){
-                                prev_pixel_x = inst_pixel_x;
-                                prev_pixel_y = inst_pixel_y;
-                          //      std::cout << "initial pose" << std::endl;
-                                check = 1;
-                        }
-                        else if(((inst_pixel_x-prev_pixel_x)*(inst_pixel_x-prev_pixel_x)+(inst_pixel_y-prev_pixel_y)*(inst_pixel_y-prev_pixel_y))>80){
-                                prev_pixel_x = inst_pixel_x;
-                                prev_pixel_y = inst_pixel_y;
-                            //    std::cout << "pixel fixed" << std::endl;
-                                check = 1;
-                        }
-                        if(check==1){
+				prev_pixel_x = inst_pixel_x;
+				prev_pixel_y = inst_pixel_y;
+				// std::cout << "initial pose" << std::endl;
+				check = 1;
+			}
+			else if(((inst_pixel_x-prev_pixel_x)*(inst_pixel_x-prev_pixel_x)+(inst_pixel_y-prev_pixel_y)*(inst_pixel_y-prev_pixel_y))>80){
+				prev_pixel_x = inst_pixel_x;
+				prev_pixel_y = inst_pixel_y;
+				// std::cout << "pixel fixed" << std::endl;
+				check = 1;
+			}
+			if(check==1){
 				cv::circle(glob_map, cv::Point(inst_pixel_x, inst_pixel_y), 2, cv::Scalar(255,0,0), -1);
-			//	std::cout << "pixel filled" << std::endl;
-                                count++;
-                        }
+				// std::cout << "pixel filled" << std::endl;
+				count++;
+			}
 			
 			if(!x_500 && !y_500 && !x_14500 && !y_14500){
 				std::cout << "on map" << std::endl;
-				for(int i=0; i<1000; i++){
-					copy_pixel_y = inst_pixel_y - 500 + i;
-					for(int j=0; j<1000; j++){
-						copy_pixel_x = inst_pixel_x - 500 + j;
-						mini_map.at<cv::Vec3b>(i, j)[0] = glob_map.at<cv::Vec3b>(copy_pixel_y, copy_pixel_x)[0];
-						mini_map.at<cv::Vec3b>(i, j)[1] = glob_map.at<cv::Vec3b>(copy_pixel_y, copy_pixel_x)[1];
-						mini_map.at<cv::Vec3b>(i, j)[2] = glob_map.at<cv::Vec3b>(copy_pixel_y, copy_pixel_x)[2];
-					}
-				}
+				mini_map = glob_map(cv::Rect(inst_pixel_x-500,inst_pixel_y-500,1000,1000));
+				// for(int i=0; i<1000; i++){
+				// 	copy_pixel_y = inst_pixel_y - 500 + i;
+				// 	for(int j=0; j<1000; j++){
+				// 		copy_pixel_x = inst_pixel_x - 500 + j;
+				// 		mini_map.at<cv::Vec3b>(i, j)[0] = glob_map.at<cv::Vec3b>(copy_pixel_y, copy_pixel_x)[0];
+				// 		mini_map.at<cv::Vec3b>(i, j)[1] = glob_map.at<cv::Vec3b>(copy_pixel_y, copy_pixel_x)[1];
+				// 		mini_map.at<cv::Vec3b>(i, j)[2] = glob_map.at<cv::Vec3b>(copy_pixel_y, copy_pixel_x)[2];
+				// 	}
+				// }
 				cv::circle(mini_map, cv::Point(500,500), 3, cv::Scalar(0,255,0), -1);
 				cv_bridge::CvImage img_bridge;
-                        	sensor_msgs::Image img_msg;
-                        	std_msgs::Header header;
-                        	img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, mini_map);
-                        	printf("image converting\n");
-                       		img_bridge.toImageMsg(img_msg);
-                        	printf("image converted!\n");
-                        	pub.publish(img_msg);
+				sensor_msgs::Image img_msg;
+				std_msgs::Header header;
+				img_bridge = cv_bridge::CvImage(header, sensor_msgs::image_encodings::RGB8, mini_map);
+				printf("image converting\n");
+				img_bridge.toImageMsg(img_msg);
+				printf("image converted!\n");
+				pub.publish(img_msg);
 			}
 		}
 };
