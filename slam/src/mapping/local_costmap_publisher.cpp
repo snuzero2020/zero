0f73843bfd8d11c615610a409c63fd6d78d2f8ed
OhJeongwoo <ohjw420@gmail.com>
2020-08-06 14:43:56 +0900

    change local costmap publisher, fix for loop range

--- slam/src/mapping/local_costmap_publisher.cpp
+++ slam/src/mapping/local_costmap_publisher.cpp
@@ -91,10 +91,10 @@ class Local_costmap_publisher{
                         cost_map.info.width = 300;
                         cost_map.info.height = 300;
 
-                        for (int i = 0; i < 300; i++){
-                                for (int j = 0; j < 300; j++) cost_map.data.push_back((int8_t)local_costmap.at<uchar>(300-i,300-j)); 
+                        for (int i = 1; i < 301; i++){
+                                for (int j = 1; j < 301; j++) cost_map.data.push_back((int8_t)local_costmap.at<uchar>(300-i,300-j)); 
                         }
-
+                        
                         cost_map_pub.publish(cost_map);
 			
 			cv_bridge::CvImage img_bridge;
