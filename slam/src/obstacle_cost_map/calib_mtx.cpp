bab6f16b3fd90f518b0e2048d29bf624ebcd3c10
goldenhazard <octoberfor97@naver.com>
2020-08-11 14:37:30 +0900

    wonsuhk_jung
    Signed-off-by: goldenhazard <octoberfor97@naver.com>

---
+++ slam/src/obstacle_cost_map/calib_mtx.cpp
@@ -0,0 +1,132 @@
+#include <opencv2/opencv.hpp>
+#include <ros/ros.h>
+#include <vector>
+#include <iostream>
+#include <string.h>
+#include "Eigen/Eigen"
+#include "geometry_msgs/Point.h"
+#include "ros/ros.h"
+#include "ros/time.h"
+#include "math.h"
+#include "slam/imgCluster.h"
+#include "slam/Clusters.h"
+#include "slam/Clustermaster.h"
+
+using namespace std;
+using namespace cv;
+using namespace Eigen;
+
+#define PI 3.14159265
+
+class Calibration{
+    public:
+    Calibration(){
+        //camera internal, external parameter Measurement
+        //Calibration.py (Mean_error= 0.0755)
+        fx = 499.65625392;
+        fy = 499.80873551;
+        cx = 317.92637171;
+        cy = 227.12706437;
+        roll = 0.0, pitch = 19.0, yaw = 0.0;
+        x = 0, y = 0.37, z = 0.2; // Unit[m] => Camera to Lidar
+
+        //Publish, Subscribe
+        pub_ = nh_.advertise<slam::imgCluster>("/pcl_on_image", 10);
+        sub_ = nh_.subscribe("/points", 1, &Calibration::callback, this);
+    }
+
+    void callback(const slam::Clusters::ConstPtr& msg){
+        internal = getInternalMatrix(fx, fy, cx, cy);
+        external = getExternalMatrix(roll, pitch, yaw, x, y, z);
+        calibration_matrix = internal * external; // final_calibration_matrix
+        cout << "Calibration_matrix" << endl;
+        cout << calibration_matrix << endl;
+        vector<slam::imgCluster> temp;
+        for(slam::Cluster clust : msg->clusters){
+            slam::imgCluster tempCluster;
+            temp.push_back(tempCluster);
+            for(geometry_msgs::Point pt : clust.points_3d){
+                tempCluster.points.push_back(lidartoImage(pt));
+            }
+        }
+        slam::Clustermaster rt;
+        rt.header = msg->header;
+        rt.clusters = temp;
+        pub_.publish(rt);
+    }
+
+    Matrix3d getInternalMatrix(double fx, double fy, double cx, double cy){
+        Matrix3d internal_;
+        internal_<< fx, 0.0f, cx,
+                    0.0f, fy, cy,
+                    0.0f, 0.0f, 1.0f;
+        cout << "Internal Matrix: " << endl;
+        cout << internal_ << endl;
+        return internal_; 
+    }
+
+    Matrix3Xd getExternalMatrix(double roll, double pitch, double yaw, double x, double y, double z){
+        Vector3d trans;
+        Matrix3Xd rotd(3,3), rotx(3,3), roty(3,3), rotz(3,3);
+        trans << x, y, z;
+        rotd << 0.0f, -1.0f, 0.0f,
+                0.0f, 0.0f, -1.0f,
+                1.0f, 0.0f, 0.0f;
+        rotx << 1.0f, 0.0f, 0.0f,
+                0.0f, cos(PI*roll/180), -sin(PI*roll/180),
+                0.0f, sin(PI*roll/180), cos(PI*roll/180);
+        roty << cos(PI*pitch/180), 0.0f, -sin(PI*pitch/180),
+                0.0f, 1.0f, 0.0f,
+                -sin(PI*pitch/180), 0.0f, cos(PI*pitch/180);
+        rotz << cos(PI*yaw/180), -sin(PI*yaw/180), 0.0f,
+                sin(PI*yaw/180), cos(PI*yaw/180), 0.0f,
+                0.0f, 0.0f, 1.0f;
+        Matrix3Xd rot(3,3);
+        rot = rotd * rotx * roty * rotz;
+        rot.conservativeResize(rot.rows(), rot.cols()+1);
+        rot.col(rot.cols()-1) = trans;
+        cout << "External Matrix: " << endl;
+        cout << rot << endl;
+        return rot;
+    }
+
+    geometry_msgs::Point lidartoImage(geometry_msgs::Point pt){
+        Vector4d lidar_pt;
+        Vector3d image_pt, camera_world;
+        lidar_pt << pt.x, pt.y, pt.z, 1.0;
+        image_pt = calibration_matrix * lidar_pt;
+        //camera_world = external * lidar_pt;
+        cout << image_pt(0) << "<" << image_pt(1) << "<" << image_pt(2) << endl;
+        //cout << camera_world(0) << "<" << camera_world(1) << "<" << camera_world(2) << endl;
+        geometry_msgs::Point rt;
+        for(int i=0; i<3 ; i++){
+            image_pt(i) /= image_pt(2);
+        }
+        rt.x = image_pt(0);
+        rt.y = image_pt(1);
+
+        return rt;
+    }
+
+    private:
+    ros::NodeHandle nh_;
+    ros::Publisher pub_;
+    ros::Subscriber sub_;
+    double fx, fy, cx, cy;
+    double roll, pitch, yaw, x, y, z;
+    Matrix3d internal;
+    Matrix3Xd external;
+    Matrix3Xd calibration_matrix;
+    vector<int> lidar_channels;
+    vector<int> lidar_clusters;
+    vector<geometry_msgs::Point> lidar_points;
+};
+
+//imgCluster :: Cluster[], Header 
+//Cluster :: Point[], int count
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "calib_matrix");
+    Calibration calibration_;
+    ros::spin();
+}
---
+++ slam/src/obstacle_cost_map/object_detector.cpp
@@ -0,0 +1,232 @@
+#include "ros/ros.h"
+#include "ros/time.h"
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <set>
+#include "slam/Lidar.h"
+#include "slam/Cluster.h"
+#include "slam/Clusters.h"
+#include "geometry_msgs/Point.h"
+#include "Eigen/Eigen"
+
+using namespace std;
+using namespace Eigen;
+
+class ObjectDetector{
+    public:
+    ObjectDetector(){
+        pub_ = nh_.advertise<slam::Clusters>("/point_cloud_clusters", 10);
+        sub_ = nh_.subscribe("/points", 1, &ObjectDetector::callback, this);
+        plane_tolerance_ = 0.05;
+        cluster_tolerance_ = 0.10;
+        cluster_threshold_ = 5;
+        lidar_angle_ = 18;
+        lidar_height_ = 1.25;
+        zero_position_.x = 0.0;
+        zero_position_.y = 0.0;
+        zero_position_.z = 0.0;
+        x_position_.x = 1.0;
+        x_position_.y = 0.0;
+        x_position_.z = 0.0;
+    }
+
+    geometry_msgs::Point projection(geometry_msgs::Point point){
+        geometry_msgs::Point rt;
+        double t = (plane_config_[0]*point.x+plane_config_[1]*point.y+plane_config_[2]*point.z+plane_config_[3])
+        /(plane_config_[0]*plane_config_[0]+plane_config_[1]*plane_config_[1]+plane_config_[2]*plane_config_[2]);
+        rt.x = point.x - plane_config_[0]*t;
+        rt.y = point.y - plane_config_[1]*t;
+        rt.z = point.z - plane_config_[2]*t;
+        return rt;
+    }
+
+    geometry_msgs::Point cross_product(geometry_msgs::Point p1, geometry_msgs::Point p2){
+        geometry_msgs::Point rt;
+        rt.x = p1.y*p2.z - p1.z*p2.y;
+        rt.y = p1.z*p2.x - p1.x*p2.z;
+        rt.z = p1.x*p2.y - p1.y*p1.x;
+        return rt;
+    }
+
+    geometry_msgs::Point normalization(geometry_msgs::Point v){
+        double t = sqrt(v.x*v.x+v.y*v.y+v.z*v.z);
+        v.x = v.x/t;
+        v.y = v.y/t;
+        v.z = v.z/t;
+        return v;
+    }
+
+    inline double get_distance(geometry_msgs::Point p1, geometry_msgs::Point p2){
+        return sqrt((p1.x-p2.x)*(p1.x-p2.x)+(p1.y-p2.y)*(p1.y-p2.y)+(p1.z-p2.z)*(p1.z-p2.z));
+    }
+
+    inline int find(int a){
+        if(clustering_helper_[a]<0) return a;
+        clustering_helper_[a] = find(clustering_helper_[a]);
+        return clustering_helper_[a];
+    }
+
+    inline void merge(int a, int b){
+        a = find(a);
+        b = find(b);
+        if(a==b) return;
+        clustering_helper_[b] = a;
+    }
+
+    void select_candidate(){
+        candidate_points_.clear();
+        int index = 0;
+        for(geometry_msgs::Point point : cloud_points_){
+            if (-point.x*sin(lidar_angle_*M_PI/180)+point.z*cos(lidar_angle_*M_PI/180) < -lidar_height_ + plane_tolerance_*0){
+                candidate_points_.push_back(index);
+            }
+            index++;
+        }
+        ROS_INFO("# of candidate points : %d",index);
+    }
+
+    void ransac_plane(){
+        plane_config_[0]=-sin(lidar_angle_*M_PI/180);
+        plane_config_[1]=0.0;
+        plane_config_[2]=cos(lidar_angle_*M_PI/180);
+        plane_config_[3]=-lidar_height_;
+        set<int> inliers_result;
+        for(int i =0;i<cloud_points_.size();i++){
+            geometry_msgs::Point point = cloud_points_.at(i);
+            if (-point.x*plane_config_[0]+point.z*plane_config_[2] < plane_config_[3] + plane_tolerance_*4){
+                inliers_result.insert(i);
+            }
+        }
+        ROS_INFO("# of inliers : %d", inliers_result.size());
+        vector<bool> check_points;
+        for(int i=0;i<cloud_points_.size();i++) check_points.push_back(false);
+        for(int index : inliers_result) check_points.at(index) = true;
+        for(int i=0;i<check_points.size();i++){
+            if(check_points.at(i)) continue;
+            filtered_points_.push_back(cloud_points_.at(i));
+            filtered_channels_.push_back(cloud_channels_.at(i));
+            clustering_helper_.push_back(-1);
+        }
+        
+    }
+
+
+    void projecting_points(){
+        geometry_msgs::Point normal_vector;
+        normal_vector.x = plane_config_[0];
+        normal_vector.y = plane_config_[1];
+        normal_vector.z = plane_config_[2];
+        geometry_msgs::Point lidar_position = projection(zero_position_);
+        geometry_msgs::Point lidar_x = projection(x_position_);
+        geometry_msgs::Point lidar_y = cross_product(normal_vector,lidar_x);
+        lidar_x = normalization(lidar_x);
+        lidar_y = normalization(lidar_y);
+        Matrix<double,3,2> A;
+        A<<lidar_x.x,lidar_y.x, lidar_x.y,lidar_y.y, lidar_x.z,lidar_y.z;
+        for(geometry_msgs::Point point : filtered_points_){
+            Matrix<double,3,1> b;
+            geometry_msgs::Point projected_point = projection(point);
+            b <<projected_point.x-lidar_position.x, projected_point.y-lidar_position.y, projected_point.z-lidar_position.z;
+            Matrix<double,2,1> solution = A.bdcSvd(ComputeThinU|ComputeThinV).solve(b);
+            geometry_msgs::Point rt;
+            rt.x = solution(0);
+            rt.y = solution(1);
+            rt.z = 0;
+            projected_points_.push_back(rt);
+        }
+    }
+
+    vector<vector<int>> clustering(){
+        int n = projected_points_.size();
+        for(int i = 0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(find(i) == find(j)) continue;
+                if(get_distance(projected_points_.at(i),projected_points_.at(j))<cluster_tolerance_){
+                    merge(i,j);
+                }
+            }
+        }
+        vector<vector<int>> storage;
+        vector<vector<int>> clusters;
+        for(int i=0;i<n;i++) {
+            vector<int> cluster;
+            storage.push_back(cluster);
+        }
+        for(int i=0;i<n;i++){
+            storage.at(find(i)).push_back(i);
+        }
+        for(int i=0;i<n;i++){
+            if(storage.at(i).size()<cluster_threshold_) continue;
+            clusters.push_back(storage.at(i));
+        }
+        return clusters;
+    }
+
+    void callback(const slam::Lidar::ConstPtr& msg){
+        clock_t begin = clock();
+        cloud_points_ = msg->points;
+        cloud_channels_ = msg->channels;
+        candidate_points_.clear();
+        filtered_points_.clear();
+        filtered_channels_.clear();
+        projected_points_.clear();
+        clustering_helper_.clear();
+
+        select_candidate();
+        ransac_plane();
+        projecting_points();
+        vector<vector<int>> clusters = clustering();
+
+        //Publish Data
+        slam::Clusters rt;
+        vector<slam::Cluster> rt_clusters;
+        vector<geometry_msgs::Point> rt_points;
+        rt.header.stamp = ros::Time::now();
+        for(vector<int> cluster : clusters){
+            slam::Cluster rt_cluster;
+            rt_cluster.count = 0;
+            for(int index : cluster){
+                rt_cluster.points_2d.push_back(projected_points_.at(index));
+                rt_cluster.points_3d.push_back(filtered_points_.at(index));
+                rt_cluster.count ++;
+            }
+            rt_clusters.push_back(rt_cluster);
+        }
+        rt.clusters = rt_clusters;
+        pub_.publish(rt);
+        clock_t end = clock();
+        ROS_INFO("# of filtered points : %d", projected_points_.size());
+        ROS_INFO("elapsed time : %lf",double(end-begin)/CLOCKS_PER_SEC);
+        ROS_INFO("cosine value between z and normal : %lf", acos(plane_config_[2]
+        /sqrt(plane_config_[0]*plane_config_[0]+plane_config_[1]*plane_config_[1]+plane_config_[2]*plane_config_[2]))*180/M_PI);
+    }
+
+    private:
+    ros::NodeHandle nh_;
+    ros::Publisher pub_;
+    ros::Subscriber sub_;
+    vector<geometry_msgs::Point> cloud_points_;
+    vector<int> cloud_channels_;
+    vector<int> candidate_points_;
+    vector<geometry_msgs::Point> filtered_points_;
+    vector<int> filtered_channels_;
+    vector<geometry_msgs::Point> projected_points_;
+    vector<int> clustering_helper_;
+    double plane_config_[4]; // 
+    double lidar_angle_;
+    double lidar_height_;
+    double plane_tolerance_;
+    double cluster_tolerance_;
+    int cluster_threshold_;
+    geometry_msgs::Point zero_position_;
+    geometry_msgs::Point x_position_;
+};
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "object_detector");
+    ObjectDetector object_detector;
+    ros::spin();
+}
