4ee2994d8bbde7429af7c8696ae46e1649f13ef5
healtykim <bsbs8645@snu.ac.kr>
2020-08-15 14:51:06 +0900

    merged k-city and FMTC

--- slam/src/map_layer/velocity_publisher.cpp
+++ slam/src/map_layer/velocity_publisher.cpp
@@ -8,6 +8,7 @@
 #include "XYToPixel.h"
 #include "std_msgs/Float64.h"
 #include <string>
+bool is_kcity;
 
 class velocity_publisher{
 
@@ -19,18 +20,30 @@ class velocity_publisher{
         bool x_inRange{(pixel_x<=14500)}, y_inRange{(pixel_y<=14500)};
         double recommended_velocity;
 
-
     public:
-        std::stringstream path_stream;
-        //cv::Mat velocity_map;
-        cv::Mat velocity_map = cv::imread("/home/healthykim/catkin_ws/src/zero/slam/config/FMTC/velocity_map.png");
-
+        //std::stringstream path_stream;
+        cv::Mat velocity_map;
         velocity_publisher(){
-	    //path_stream << ros::package::getPath("slam") << "/config/FMTC/velocity_map.png";
-        //cv::Mat velocity_map = cv::imread(path_stream.str());
-
+        if(is_kcity==true){
+            cv::Mat velocity_map = cv::imread("/home/healthykim/catkin_ws/src/zero/slam/config/KCity/KCity_velocity_map.png");
+        }
+        else if(is_kcity==false){
+            cv::Mat velocity_map = cv::imread("/home/healthykim/catkin_ws/src/zero/slam/config/FMTC/FMTC_velocity_map.png");
+        }
         ROS_INFO("Image loaded");
 
+        /*
+        if(is_kcity==true){
+    	    path_stream << ros::package::getPath("slam") << "/config/KCity/KCityvelocity_map.png";
+            cv::Mat velocity_map = cv::imread(path_stream.str());  
+        }
+        else if(is_kcity==false){
+    	    path_stream << ros::package::getPath("slam") << "/config/FMTC/FMTC_velocity_map.png";
+            cv::Mat velocity_map = cv::imread(path_stream.str()); 
+        }
+        ROS_INFO("Image loaded");
+
+        */
         pub = nh.advertise<std_msgs::Float64>("/recommended_velocity", 2);
         sub = nh.subscribe("/filtered_data",2, &velocity_publisher::callback, this);
         }
@@ -41,7 +54,6 @@ class velocity_publisher{
 
          if(x_inRange&&y_inRange){
              std::cout<<"on map"<<std::endl;
-             //publish the information of a pixel
              recommended_velocity = velocity_map.at<cv::Vec3b>(pixel_x, pixel_y)[0];
              std_msgs::Float64 rt;
              rt.data = recommended_velocity/85;
@@ -52,6 +64,7 @@ class velocity_publisher{
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "velocity_publisher");
+    ros::param::get("/is_kcity", is_kcity);
     velocity_publisher velocity_publisher;
     ros::spin();
 }
