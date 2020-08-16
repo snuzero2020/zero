71e225dfb3589f3304680fabbecbc51e55fb5adc
sunho001215 <sunho001215@gmail.com>
2020-08-16 14:42:52 +0900

    update lanenet_lane_detection cmake files

--- computer_vision/lanenet_lane_detection/src/LaneNet_seg_subscriber.cpp
+++ computer_vision/lanenet_lane_detection/src/LaneNet_seg_subscriber.cpp
@@ -1,21 +1,7 @@
 #include <ros/ros.h>
-#include <std_msgs/UInt8MultiArray.h>
 #include <opencv2/opencv.hpp>
-
-void seg_print_callback(const std_msgs::UInt8MultiArray::ConstPtr &msg){
-    cv_bridge::CvImagePtr cv_ptr;
-    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    cv::Mat seg_img = cv_ptr->image;
-    cv::imshow("seg_img", seg_img);
-    cv::waitKey(1);
-}
-
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "LaneNet_subscriber_node");
-    ros::NodeHandle nh;
-    ros::Subscriber seg_sub = nh.subscribe("/lane_seg_topic", 1, &seg_print_callback);
-    ros::spin();
     return 0;
 }
