dc17afb9a4c73e418b68b021be9bde666dd08a32
healtykim <bsbs8645@snu.ac.kr>
2020-08-17 16:33:16 +0900

    modified

--- slam/src/map_layer/sector_publisher.cpp
+++ slam/src/map_layer/sector_publisher.cpp
@@ -86,11 +86,17 @@ class sector_publisher{
                 //           D ==> pub 3
 
                 std_msgs::UInt32 rt;
+
                 if(nBlue==0&&nGreen==0&&nRed==0)
                 {
                     rt.data = 0;
                     ROS_INFO("Sector A");
                 }
+                if(nBlue==255&&nGreen==255&&nRed==255){
+                    rt.data = 0;
+                    ROS_INFO("Sector A");
+                }
+
                 else if(nGreen==255){
                     if(nBlue == 0 && nRed ==0){
                         rt.data = 2;
@@ -100,13 +106,25 @@ class sector_publisher{
                         rt.data = 5;
                         ROS_INFO("Sector F");
                     }
+                    if(nRed == 100 && nBlue == 140){
+                        rt.data = 5|(1<<4);
+                        ROS_INFO("Sector F'");
+                    }
                     if(nRed == 140 && nBlue ==0){
                         rt.data = 7;
                         ROS_INFO("Sector H");
                     }
+                    if(nRed == 140 && nBlue == 100){
+                        rt.data = 7|(1<<4);
+                        ROS_INFO("Sector H'");
+                    }
                     if(nRed == 0 && nBlue == 255){
                         rt.data = 11;
                         ROS_INFO("Sector L");
+                    }
+                    if(nRed == 100 && nBlue == 255){
+                        rt.data = 11|(1<<4);
+                        ROS_INFO("Sector L'");
                     }
                 }
                 
@@ -124,9 +142,17 @@ class sector_publisher{
                         rt.data = 8;
                         ROS_INFO("Sector I");
                     }
+                    if(nGreen == 140 && nRed == 100){
+                        rt.data = 8|(1<<4);
+                        ROS_INFO("Sector I'");
+                    }
                     if(nRed == 140 && nGreen == 0){
                         rt.data = 9;
                         ROS_INFO("Sector J");
+                    }
+                    if(nRed == 140 && nGreen == 100){
+                        rt.data = 9|(1<<4);
+                        ROS_INFO("Sector J'");
                     }
                 }
 
@@ -141,15 +167,29 @@ class sector_publisher{
                         rt.data = 4;
                         ROS_INFO("Sector E");
                     }
+                    if(nBlue==100 && nGreen==140){
+                        rt.data = 4|(1<<4);
+                        ROS_INFO("Sector E'");
+                    }
+
                     if(nBlue==140 && nGreen==0){
                         rt.data = 6;
                         ROS_INFO("Sector G");
                     }
-                    if(nGreen==255){
+                    if(nBlue==140 && nGreen == 100){
+                        rt.data = 6|(1<<4);
+                        ROS_INFO("Sector G'");
+                    }
+                    if(nGreen==255 && nBlue == 0){
                         rt.data = 10;
                         ROS_INFO("Sector K");
                     }
+                    if(nGreen==255 && nBlue == 100){
+                        rt.data = 10|(1<<4);
+                        ROS_INFO("Sector K'");
+                    }
                 }
+                 std::cout<<"Published: "<<rt.data<<std::endl;
                  pub.publish(rt);
             }
         }
