64bbf0d7d1d1d6f5bd6603adb4d3d3876ed8f8b5
healtykim <bsbs8645@snu.ac.kr>
2020-08-29 10:23:16 +0900

    velocity_map modified

--- slam/src/map_layer/color_velocity.cpp
+++ slam/src/map_layer/color_velocity.cpp
@@ -22,11 +22,11 @@ int main(int argc, char**argv){
     bool is_kcity;
 
     ros::param::get("/is_kcity", is_kcity);
-    is_kcity = false;
+    is_kcity = true;
     if(is_kcity==true){
-       path_stream1 << ros::package::getPath("slam") << "/config/KCity/KCity_color_map.png";
+       path_stream1 << ros::package::getPath("slam") << "/config/KCity/KCity_color_map_v.png";
        path_stream2 << ros::package::getPath("slam")<<"/config/KCity/KCity_discrete_velocity_map.png";
-       path_stream3 << ros::package::getPath("slam")<<"/config/KCity/linear/KCity_velocity_map_167.png";
+       path_stream3 << ros::package::getPath("slam")<<"/config/KCity/KCity_velocity_map_555_4.png";
        color_map = imread(path_stream1.str());
        if(!color_map.empty()){
           ROS_INFO("kcity color map loaded");
@@ -46,7 +46,8 @@ int main(int argc, char**argv){
     resize(discrete_velocity_map, discrete_velocity_map, Size(color_map.cols, color_map.rows), 0, 0, CV_INTER_NN);
     resize(velocity_map, velocity_map, Size(color_map.cols, color_map.rows),0,0,CV_INTER_NN);
 
-    //FMTC
+    //5
+    /*
     for(int j=0; j<color_map.cols; j++){
         for(int i=0; i<color_map.rows; i++){
              int nBlue, nGreen, nRed;
@@ -99,141 +100,47 @@ int main(int argc, char**argv){
              discrete_velocity_map.at<cv::Vec3b>(i,j)[0]= 85;             
         }
     }    
-
-    /*
-    //convert
+*/
+    //4 at KCity
     for(int j=0; j<color_map.cols; j++){
         for(int i=0; i<color_map.rows; i++){
              int nBlue, nGreen, nRed;
              nBlue = color_map.at<Vec3b>(i, j)[0];
              nGreen = color_map.at<Vec3b>(i, j)[1];
              nRed = color_map.at<Vec3b>(i, j)[2];
-
-
+            
             if(nBlue==0&&nGreen==0&&nRed==0){
                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                //Sector A
+                //Driving section
             }
-            ////////
             else if(nBlue==255&&nGreen==255&&nRed==255){
                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                //Sector A, with curve
             }
-
             else if(nGreen==255){
-                if(nBlue == 0 && nRed ==0){
+                if(nRed == 255 && nBlue == 0){
                  discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 127;
-                  //Sector C
-                }
-                if(nRed == 0 && nBlue == 140){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 127;
-                  //Sector F
-                }
-                if(nRed == 100 && nBlue == 140){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                 //Sector F'
-                }
-                if(nRed == 140 && nBlue ==0){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 127;
-                 //Sector H
-                }
-                if(nRed == 140 && nBlue == 100){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                 //Sector H'
-                }
-                
-                //KCity 
-                
-                if(nRed == 0 && nBlue == 255){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 127;
-                 //Sector L;
-                }
-                
-                if(nRed == 0 && nBlue == 255){
-                    discrete_velocity_map.at<cv::Vec3b>(i,j)[0]=226;
-                }
-                // 4 -> 1.5
-
-                //only FMTC
-                if(nBlue == 140 && nRed == 140){
-                    discrete_velocity_map.at<cv::Vec3b>(i,j)[0]=170;
-                }
-
-                if(nRed == 100 && nBlue == 255){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                 //Sector L';
+                 //intersection buffer 2
                 }
             }
-                
+
             else if(nBlue==255){
                 if(nRed==0 && nGreen==0){
                  discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 170;
-                 //Sector D
+                 //intersection buffer 3
                 }
                 if(nGreen==100 && nRed==100){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 127;
-                 //Sector D'
+                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 106;
+                 //intersection
                 }
-                if(nGreen == 140 && nRed == 0){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 127;
-                 //Sector I
-                }
-                if(nGreen == 140 && nRed == 100){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                 //Sector I'
-                }
-                if(nRed == 140 && nGreen == 0){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 127;
-                 //Sector J
-                }
-                if(nRed == 140 && nGreen == 100){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                 //Sector J'
-                }
-            }
+            } 
 
-            else if(nRed==255)
-            {
-                ////////////
-                if(nBlue==0 && nGreen==0){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                 //Sector B
-                }
-                
-                if(nBlue==0 && nGreen==140){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 127;
-                 //Sector E
-                }
-                if(nBlue==100 && nGreen==140){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                 //Sector E'
-                }
+            else
+             discrete_velocity_map.at<cv::Vec3b>(i,j)[0]= 85;             
+        }
+    }    
 
-                if(nBlue==140 && nGreen==0){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 127;
-                 //Sector G
-                }
-                if(nBlue==140 && nGreen == 100){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                 //Sector G'
-                }
-                if(nGreen==100 && nBlue == 100){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 127;
-                 //Sector K
-                }
-                ////////
-                if(nGreen==100 && nBlue == 0){
-                 discrete_velocity_map.at<cv::Vec3b>(i, j)[0] = 85;
-                 //Sector K'
-                //////
-            }
-                if(nRed == 255 && nGreen == 255 && nBlue == 0){
-                    discrete_velocity_map.at<cv::Vec3b>(i,j)[0]=106;
-                }
 
-        } 
-    }
-    */
+
 
     //cv::Mat kernel=cv::getStructuringElement(cv::MORPH_RECT, cv::Size(99,99));
     //cv::dilate(discrete_velocity_map, discrete_velocity_map, kernel, cv::Point(-1,-1), 1);
