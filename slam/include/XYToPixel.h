785bf00de07fb298fd9696efb8e2c8d39ed68e00
Dongha Hwang <depth221@gmail.com>
2021-05-06 23:50:44 +0900

    add comments into XYToPixel.h

--- slam/include/XYToPixel.h
+++ slam/include/XYToPixel.h
@@ -9,7 +9,29 @@
 
 #endif
 
+/**
+@brief UTM52N 좌표(x, y)를 지도 픽셀 값(pixel_x, pixel_y)로 변환한다.
+
+변환된 픽셀 값이 실제 지도의 범위를 벗어나는지는 고려하지 않는다(= 유효성 검사를 하지 않는다).
+
+@param pixel_x 픽셀 x 좌표를 받아올 변수.
+@param pixel_y 픽셀 y 좌표를 받아올 변수.
+@param x 변환할 UTM52N x 좌표.
+@param y 변환할 UTM52N y 좌표.
+@param is_kcity 장소. true는 K-City를, false는 FMTC를 나타낸다.
+*/
 void XYToPixel(int& pixel_x, int& pixel_y, double x, double y, bool is_kcity);
+/**
+@brief 지도 픽셀 값(pixel_x, pixel_y)을 UTM52N 좌표(x, y)로 변환한다.
+
+@remark 변환된 좌표 값이 실제 지도의 범위를 벗어나는지는 고려하지 않는다. 유효성 검사는 직접 해야 한다.
+
+@param x UTM52N x 좌표를 받아올 변수.
+@param y UTM52N y 좌표를 받아올 변수.
+@param pixel_x 변환할 픽셀 x 좌표.
+@param pixel_y 변환할 픽셀 y 좌표.
+@param is_kcity 장소. true는 K-City를, false는 FMTC를 나타낸다.
+*/
 void PixelToXY(double& x, double& y, int pixel_x, int pixel_y, bool is_kcity);
 
 //int XYToPixel(cv::Mat img, double x, double y, int& pixel_x, int& pixel_y, int place);
